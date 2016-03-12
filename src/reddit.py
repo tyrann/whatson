d5472f0e5032316e67882dd575616f9d3c01e181
Timothée Lottaz <timozattol@gmail.com>
2016-03-12 02:24:00 +0100

    Docstring

--- src/reddit.py
+++ src/reddit.py
@@ -1,6 +1,7 @@
 import praw
 
 def get_comments(reddit_username):
+    """ Get last comments of user "reddit_username" """
     user_agent = ("User tone analysis")
     reddit = praw.Reddit(user_agent=user_agent)
     objects_limit = 10
