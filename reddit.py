7cfbb17dd019640fade0a1352aba1ee219b9d977
Timothée Lottaz <timozattol@gmail.com>
2016-03-12 02:21:09 +0100

    Reddit comment fetch

---
+++ reddit.py
@@ -0,0 +1,11 @@
+import praw
+
+def get_comments(reddit_username):
+    user_agent = ("User tone analysis")
+    reddit = praw.Reddit(user_agent=user_agent)
+    objects_limit = 10
+    user = reddit.get_redditor(reddit_username)
+
+    comments = user.get_comments(limit=objects_limit)
+
+    return comments
