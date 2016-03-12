049816cb27197782e472bd78b0d52ec979d4b1fa
tyrann <thomas.j.rubeli@gmail.com>
2016-03-12 04:33:11 +0100

    Add emotion plot

---
+++ src/plot.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot(anger, disgust, fear, joy, sadness):
+    emotions = ("Anger", "Disgust", "Fear", "Joy", "Sadness")
+    y_pos = np.arange(len(emotions))
+    performance = (anger, disgust, fear, joy, sadness)
+    error = np.random.rand(len(emotions))
+    
+    plt.barh(y_pos, performance, align='center', alpha=0.4)
+    plt.yticks(y_pos, emotions)
+    plt.xlabel('Intensity')
+    plt.title('Emotional intensities')
+    
+    plt.show()
