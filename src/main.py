7c8866e81f3f57e677e4a14f6f5eacb1cbefaa09
Timothée Lottaz <timozattol@gmail.com>
2016-03-12 03:21:27 +0100

    Create get_mood_for_user skeleton

--- src/main.py
+++ src/main.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
+from reddit import get_comments
+from watson_queries import query_watson
+
+def get_mood_for_user(username, subreddit=None, comments_limit=1):
+    """ Compute watson mood of user "username" on subreddit "subreddit" by
+    looking at last comments. """
+
+    comments = [str(x) for x in get_comments(username, comments_limit)]
+
+    watson_queries = []
+    for comment in comments:
+        watson_queries.append(query_watson(comment))
+
+    return watson_queries
 
 def main():
-    pass
+    [print(x) for x in get_mood_for_user("timozattol")]
 
 if __name__ == '__main__':
     main()
--- src/reddit.py
+++ src/reddit.py
@@ -1,12 +1,11 @@
 import praw
 
-def get_comments(reddit_username):
+def get_comments(reddit_username, comments_limit=1):
     """ Get last comments of user "reddit_username" """
     user_agent = ("User tone analysis")
     reddit = praw.Reddit(user_agent=user_agent)
-    objects_limit = 10
     user = reddit.get_redditor(reddit_username)
 
-    comments = user.get_comments(limit=objects_limit)
+    comments = user.get_comments(limit=comments_limit)
 
     return comments
